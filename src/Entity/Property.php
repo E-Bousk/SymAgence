77be5bf6262dcfed8bed9194edd17bb39c024331
E.Bousk <e.bousk@gmail.com>
2022-01-20 21:05:15 +0100

    Use validations on property entity

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=PropertyRepository::class)
+ * @UniqueEntity("title")
  */
 class Property
 {
@@ -24,6 +27,7 @@ class Property
     private $id;
 
     /**
+     * @Assert\Length(min=5, max=255)
      * @ORM\Column(type="string", length=255)
      */
     private $title;
@@ -34,6 +38,7 @@ class Property
     private $description;
 
     /**
+     * @Assert\Range(min=10, max=400)
      * @ORM\Column(type="integer")
      */
     private $surface;
@@ -74,6 +79,7 @@ class Property
     private $address;
 
     /**
+     * @Assert\Regex("/^[0-9]{5}$/")
      * @ORM\Column(type="string", length=255)
      */
     private $postal_code;
